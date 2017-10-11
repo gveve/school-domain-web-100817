3d36eb84cb73ed670831275629cbf498fccc6417
github username <github email address>
2017-10-11 14:59:57 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -14,7 +14,6 @@ attr_accessor :school_name, :roster
         @roster[student_grade] = []
         @roster[student_grade] << student_name
     end
-    # @roster.each{|student_name, student_grade| @roster[student_grade]<< student_name}
   end
 
   def grade (student_grade)
